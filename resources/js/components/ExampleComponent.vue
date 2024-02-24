9468c4f095a329ada9e3c564503c444e257df4a7
Dom304 <dominiquesimmons304@gmail.com>
2024-02-24 14:49:41 -0500

    inital commit (broken)

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="changeMessage">Change Message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello, World!"
+    };
+  },
+  methods: {
+    changeMessage() {
+      this.message = "New Message";
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+button {
+  background-color: yellow;
+  color: black;
+}
+</style>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vue 3 Test</title>
+</head>
+<body>
+    <div>
+        <h1>Vue 3 Test</h1>
+    </div>
+
+    <div id="app">
+        <example-component></example-component>
+    </div>
+</body>
+</html>
